9b54c2328ba19ec9f81194df403bc5c1d65c13d6
Muhammad Salah <muhammadsalah@tqweem.com>
2020-07-20 20:16:49 +0200

    Stripping the application to Medicare labeling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AppComponent} from "./app.component";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+   path: '',
+   redirectTo: 'app',
+   pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'app',
+    component: AppComponent
   }
 ];
 

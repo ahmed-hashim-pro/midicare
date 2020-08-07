5dc8cc072e1c6f56f178ada5a0a1fc2318282a31
Muhammad Salah <muhammadsalah@tqweem.com>
2020-08-07 19:10:55 +0200

    Updating error handler on sign in process

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '@core/service/auth/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {LayoutService} from '@core/service/theme/layout.service';
 import {NgForm} from '@angular/forms';
+import {ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,7 @@ export class LoginComponent implements OnInit {
   private submitted: boolean;
 
   constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute,
-              private layoutService: LayoutService) { }
+              private layoutService: LayoutService, private toastController: ToastController) { }
 
   ngOnInit() {
     this.returnURL = this.route.snapshot.queryParams['returnURL'] || 'app';
@@ -33,11 +34,31 @@ export class LoginComponent implements OnInit {
   async signIn(form: NgForm) {
     this.submitted = true;
     if (form.valid) {
-      await this.authService.signIn(this.login.username, this.login.password);
-      if (this.returnURL) {
-        await this.router.navigateByUrl(this.returnURL);
-      } else {
-        await this.router.navigateByUrl('');
+      try {
+        await this.authService.signIn(this.login.username, this.login.password);
+        if (this.returnURL) {
+          await this.router.navigateByUrl(this.returnURL);
+        } else {
+          await this.router.navigateByUrl('');
+        }
+      } catch (e) {
+        await (await this.toastController.create(
+            {
+              message: 'Username or password is not accepted by the system',
+              mode: 'md',
+              duration: 5000,
+              position: 'middle',
+              keyboardClose: true,
+              animated: true,
+              buttons: [
+                {
+                  text: 'Dismiss',
+                  icon: 'remove-circle',
+                  role: 'cancel'
+                }
+              ]
+            }
+        )).present();
       }
     }
   }
